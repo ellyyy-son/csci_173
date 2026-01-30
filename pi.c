555425d35f52638cb541855f2c51fdb5d6c3ea42
ellyyy-son <ellysonolegario@gmail.com>
2026-01-31 01:00:48 +0800

    SS

--- pi.c
+++ pi.c
@@ -1,36 +1,45 @@
-#include <mpi.h>
+#include "mpi.h"
 #include <math.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(argc,argv)
+int argc;
+char *argv[];
+
 {
-    int myid, numprocs, i;
-    long n = 100000000;
+    int done = 0, myid, numprocs, i;
+    long n;
     double PI25DT = 3.141592653589793238462643;
     double mypi, pi, x;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    n=100000000;
 
-    MPI_Bcast(&n, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD,&myid);
 
-    x = 0.0;
-    for (i = myid; i < n; i += numprocs) {
-        double sign = (i % 2 == 0) ? 1.0 : -1.0;
-        x += sign / (2.0 * i + 1.0);
+    while (!done)
+    {
+        MPI_Bcast(&n, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+        if (n == 0) break;
+
+        x = 0.0;
+        for (i = myid; i < n; i += numprocs) {
+            x += (pow(-1, i) / (2*i + 1));
+        }
+        mypi = x;
+
+        MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0,
+                   MPI_COMM_WORLD);
+
+        pi = pi * 4;
+
+        if (myid == 0){
+            printf("pi is approximately %.16f, Error is %.16f\n",
+                   pi, fabs(pi - PI25DT));
+            done = 1;
+		}
     }
-
-    mypi = x;
-
-    MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    if (myid == 0) {
-        pi = pi * 4.0;
-        printf("pi is approximately %.16f, Error is %.16f\n",
-               pi, fabs(pi - PI25DT));
-    }
-
     MPI_Finalize();
     return 0;
 }
